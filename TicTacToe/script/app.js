f699417b78aa7a268fd39463954228e421e97a91
Oh Ga Eun <dhrkdms4113@naver.com>
2023-01-02 22:11:25 +0900

    feat: 플레이어 턴 넘기기, 게임보드 기능

--- TicTacToe/script/app.js
+++ TicTacToe/script/app.js
@@ -1,13 +1,14 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
     name: '',
-    Symbol: 'X',
+    symbol: 'X',
   },
   {
     name: '',
-    Symbol: 'O',
+    symbol: 'O',
   },
 ];
 
@@ -15,10 +16,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElemnt = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn'); 
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn'); 
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -27,3 +33,10 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElemnt.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements){
+//   gameFieldElement.addEventListener('click', selectGameField);
+// }
+gameBoardElement.addEventListener('click', selectGameField);
